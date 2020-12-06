8cae1e228c3cd10bdeeb03c0219243fcfe382fb3
Doni Rubiagatra <doni@zero-one-group.com>
2020-12-06 11:01:24 +0700

    Refactor for using functional component

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -1,5 +1,25 @@
 import React from "react";
 
+function LanguagesNav({ selected, onUpdateLanguage }) {
+  const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
+
+  return (
+    <ul className="flex-center">
+      {languages.map((language) => (
+        <li key={language}>
+          <button
+            className="btn-clear nav-link"
+            onClick={() => onUpdateLanguage(language)}
+            style={language === selected ? { color: "rgb(187, 46, 31)" } : null}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 export default class Popular extends React.Component {
   constructor(props) {
     super(props);
@@ -18,24 +38,12 @@ export default class Popular extends React.Component {
   }
 
   render() {
-    const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
+    const { selectedLanguage } = this.state;
 
     return (
-      <ul className="flex-center">
-        {languages.map((language) => (
-          <li key={language}>
-            <button
-              className="btn-clear nav-link"
-              onClick={() => this.updateLanguage(language)}
-              style={
-                language === this.state.selectedLanguage ? { color: "rgb(187, 46, 31)" } : null
-              }
-            >
-              {language}
-            </button>
-          </li>
-        ))}
-      </ul>
+      <React.Fragment>
+        <LanguagesNav selected={selectedLanguage} onUpdateLanguage={this.updateLanguage} />
+      </React.Fragment>
     );
   }
 }
