b2eff852312ac566876031197fb69caef98581cc
Doni Rubiagatra <doni@zero-one-group.com>
2020-12-06 10:23:01 +0700

    Modified css and track Popular state

---
+++ github-battle/app/components/Popular.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: "All",
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage,
+    });
+  }
+
+  render() {
+    const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
+
+    return (
+      <ul className="flex-center">
+        {languages.map((language) => (
+          <li key={language}>
+            <button
+              className="btn-clear nav-link"
+              onClick={() => this.updateLanguage(language)}
+              style={
+                language === this.state.selectedLanguage ? { color: "rgb(187, 46, 31)" } : null
+              }
+            >
+              {language}
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
